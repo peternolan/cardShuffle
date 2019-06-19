6b6738a8c4aa328caa8566d1d5b7fa4cc67199c3
peternolan <pn7925@gmail.com>
2019-06-18 21:03:27 -0400

    Updated UI

--- app/controllers.js
+++ app/controllers.js
@@ -42,12 +42,34 @@ angular.module('cardShuffle.controllers', []).controller('cardsController', func
     let origDeckArr = deck;//Keep the original deck aside, in case we want to reset the current deck.
 
     /**
-     * First Method of shuffling the deck. In this method, we:
+     * Alternative way of shuffling the deck. This method provides much more random results.
+     * The deck is shuffled using the Fisher--Yates (Knuth) Shuffle Algorithm.
+     */
+    $scope.shuffle = function () {
+        let deck2 = $scope.cardList;//Holds the current cards.
+        let finalDeck = [];
+        /*
+        In this for loop, we swap two random numbers between 0 and i within the deck.
+        */
+        for (let k = 0; k < deck2.length; k++) {
+            finalDeck.push(deck2[k]);
+        }
+        for (let i = 0; i < deck2.length; i++) {
+            let j = Math.floor(Math.random() * i);
+            let temp = finalDeck[i];
+            finalDeck[i] = finalDeck[j];
+            finalDeck[j] = temp;
+        }
+        $scope.cardList = finalDeck;
+    };
+
+    /**
+     * Second Method of shuffling the deck. In this method, we:
      *      Divide the deck into two halves.
      *      Alternate between adding one card from each deck
      *      Continue the process until we have reached the end of the deck.
      */
-    $scope.shuffle = function () {
+    $scope.shuffle2 = function () {
         let deck1 = $scope.cardList; //Variable to hold the current deck.
         let finalDeck = []; //Variable that will hold out final, shuffled deck.
         //We repeat this process 7 times in order to get a much more varied solution.
@@ -81,27 +103,6 @@ angular.module('cardShuffle.controllers', []).controller('cardsController', func
         $scope.cardList = finalDeck;//Return the finalDeck as the new deck on the page.
     };
 
-    /**
-     * Alternative way of shuffling the deck. This method provides much more random results.
-     * The deck is shuffled using the Fisher--Yates (Knuth) Shuffle Algorithm.
-     */
-    $scope.shuffle2 = function () {
-        let deck2 = $scope.cardList;//Holds the current cards.
-        let finalDeck = [];
-        /*
-        In this for loop, we swap two random numbers between 0 and i within the deck.
-        */
-        for (let k = 0; k < deck2.length; k++) {
-            finalDeck.push(deck2[k]);
-        }
-        for (let i = 0; i < deck2.length; i++) {
-            let j = Math.floor(Math.random() * i);
-            let temp = finalDeck[i];
-            finalDeck[i] = finalDeck[j];
-            finalDeck[j] = temp;
-        }
-        $scope.cardList = finalDeck;
-    };
     /**
      * Reset the deck to its original order.
      */
